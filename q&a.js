226ca5f03a07dec12b8e777c7d8e24b887514fdf
Pat Quilty <p.quilty@yahoo.com>
2019-04-23 20:02:30 -0400

    initial commit

---
+++ q&a.js
@@ -0,0 +1,112 @@
+const TheQuiz = [
+    {
+      answers: [
+        'Sturdy Foothill Pose',
+        'Mountain Pose',
+        'Upright Forward Pose',
+        'Standing Corpse Pose'
+        ],
+      correctAnswer: 1,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c3b5ff691db03f7b4578521_Dimensions-Guide-Humans-Yoga-Poses-Mountain-Dimensions.svg',
+      alt: 'People standing with feet hips-width apart, a slight bend in their knees, hands by their sides and palms facing forward.'
+    },
+    {
+      answers: [
+        'Falling Down Pose',
+        'Flamingo Pose',
+        'Balanced Wind Pose',
+        'Tree Pose'
+        ],
+      correctAnswer: 3,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c3b65eee76e086ab4f27185_Dimensions-Guide-Humans-Yoga-Tree-Pose-Dimensions.svg',
+      alt: 'People standing on one leg with their raised leg bent and foot pressed to their inner thigh.'
+    },
+    {
+      answers: [
+        'Upward-Stretching Fox Pose',
+        'Triangle Pose',
+        'Downward-Facing Dog Pose',
+        'Downward Angle Pose'
+        ],
+      correctAnswer: 2,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c3b5f21652ba07c49650280_Dimensions-Guide-Humans-Yoga-Poses-Downward-Facing-Dog-Dimensions.svg',
+      alt: 'People posed with feet grounded hips-width apart opposite palms grounded shoulder-width apart with hips raised high between.'
+    },
+    {
+      answers: [
+        'Warrior I Pose',
+        'Proclamation of Innocence Pose',
+        'Raised Lunge Pose',
+        'Eagle Pose'
+        ],
+      correctAnswer: 0,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5be872ce130794052704d27c_Dimensions-Guide-Humans-Yoga-Poses-Warrior-1-Dimensions.svg',
+      alt: 'People standing with feet far apart and facing their front bent knee with hands raised above their head.'
+    },
+    {
+      answers: [
+        'Warrior II Pose',
+        'Triangle Pose',
+        'Mankey Pose',
+        'Turtle Pose'
+        ],
+      correctAnswer: 1,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c3b695608173340f3646abc_Dimensions-Guide-Humans-Yoga-Poses-Triangle-Pose-Diagram-Dimensions.svg',
+      alt: 'People standing with feet far apart, legs straight, bend from the hip towards the forward facing foot with their forward hand down and the other arm straight up.'
+    },
+    {
+      answers: [
+        'Easy Pose',
+        'Medium-Hardness Pose',
+        'Difficult Pose',
+        'Impossible Pose'
+        ],
+      correctAnswer: 0,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5bb1a3c15709136c0d1bfaf2_Dimensions-Guide-Humans-Sitting-Cross-Legged-Hero-Dimensions.svg',
+      alt: 'People sitting upright with legs crossed.'
+    },
+    {
+      answers: [
+        'Starting Pose',
+        'Outstretched Board Pose',
+        'Downward-Frisking Pose',
+        'Plank Pose'
+        ],
+      correctAnswer: 3,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c3b5fac652ba02db46502c3_Dimensions-Guide-Humans-Yoga-Poses-Plank-Dimensions.svg',
+      alt: 'People posed in an upward pushup position with straight arms perpendicular to the ground, a straight line from shoulders to heels and further supported by the toes.'
+    },
+    {
+      answers: [
+        'Half Fold Pose',
+        'Spinal Flexion Pose',
+        'Seated Forward Fold Pose',
+        'Hi Knee Pose'
+        ],
+      correctAnswer: 2,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c3b5f5e652ba0354d6502a7_Dimensions-Guide-Humans-Yoga-Poses-Seated-Forward-Bend-Dimensions.svg',
+      alt: 'People seated with legs stretched straight in front of them and torsos folded forward over the legs with hands towards the feet.'
+    },
+    {
+      answers: [
+        'Warrior XXXVII Pose',
+        'Lengthened Viper Pose',
+        'Skyward Raised Pose',
+        'Warrior II Pose'
+        ],
+      correctAnswer: 3,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5be8730c0c11cf0a3db4223a_Dimensions-Guide-Humans-Yoga-Poses-Warrior-2-Dimensions.svg',
+      alt: 'People standing with feet far apart, facing their front bent knee, and arms parallel to the ground while in line with legs.'
+    },
+    {
+      answers: [
+        'Hi Knee II Pose',
+        'Pigeon Pose',
+        'Childs Pose',
+        'Grounding Pose'
+        ],
+      correctAnswer: 2,
+      icon: 'https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c3b5ec3652ba00903650255_Dimensions-Guide-Humans-Yoga-Poses-Child%27s-Pose-Dimensions.svg',
+      alt: 'People posed with hips resting on heels, bent forward over knees with arms stretched forward across the ground.'
+    }
+];
---
+++ script.js
@@ -0,0 +1,164 @@
+let questionNumber = 0;
+let currentScore = 0;
+let answered = 0;
+
+function displayQuestion() {
+  $('.question-form').html(`
+  <legend>Name the yoga pose:</legend>
+  <label><input type="radio" name="answer" class="radio" value="0"/>${TheQuiz[questionNumber].answers[0]}<br/></label>
+  <label><input type="radio" name="answer" class="radio" value="1"/>${TheQuiz[questionNumber].answers[1]}<br/></label>
+  <label><input type="radio" name="answer" class="radio" value="2"/>${TheQuiz[questionNumber].answers[2]}<br/></label>
+  <label><input type="radio" name="answer" class="radio" value="3"/>${TheQuiz[questionNumber].answers[3]}<br/></label>
+  <button type="button" class="submit">Submit</button>
+  `);
+  // Inserts Question
+}
+function updateQuestionScreen() {
+  $('.pose').prop("src", TheQuiz[questionNumber].icon);
+  $('.pose').prop("alt", TheQuiz[questionNumber].alt);
+  // Updates question picture and alt text
+
+  questionNumber++;
+  $('.js-question').html(`Question: ${questionNumber}`);
+  // Updates and displays question number
+
+  $('.the-answer').html('');
+  // Clears the previous answer
+}
+function nextQuestion() {
+  displayQuestion();
+  // Displays question text
+
+  updateQuestionScreen()
+  // Updates the rest of the question screen
+}
+
+function displayAnswer() {
+  let correct = TheQuiz[questionNumber - 1].correctAnswer;
+  let guessed = $('input[name=answer]:checked').val();
+  // Gather correct and guessed answers
+
+  if (correct == guessed) {
+    currentScore++;
+    $('.the-answer').html(`
+    <h3>Good Job! The correct answer is: ${TheQuiz[questionNumber - 1].answers[correct]}.  You choose right!</h3>
+    `);
+  } else {
+    $('.the-answer').html(`
+    <h3>Sorry, the correct answer is: ${TheQuiz[questionNumber - 1].answers[correct]}.</h3>
+    `);
+  } // Display correct answer and message
+}
+function updateAnswerScreen() {
+  $('.question-form').html('<button type="button" class="submit">Next Question</button>');
+  // Clears the question radio buttons
+
+  answered++;
+  $('.js-score').html(`Score: ${currentScore}/${questionNumber}`);
+  // Updates and displays answered number
+
+  if (answered == 10) {
+    $(".submit").html('View Results');
+  } // Updates button text
+}
+function checkAnswer() {
+  displayAnswer();
+  // Displays answer text
+
+  updateAnswerScreen();
+  // Updates the rest of the answer screen
+}
+
+function handleButtonPress() {
+  // Directs the progress of the quiz
+  $('form').on('click', '.submit', function(event) {
+    event.preventDefault();
+
+    if (questionNumber == 0) {
+      $('.js-score').html(`Score: 0/0`);
+    } // Resets score for play again
+
+    if (answered < 10) {
+      if (answered == questionNumber) {
+        // If ready for question
+        nextQuestion();
+      } else {
+        if ($('input[name=answer]:checked').val() == undefined) {
+          // If they left a question unanswered
+          alert('Please select an answer.');
+        } else {
+          // If ready for answer
+          checkAnswer();
+        }
+      }
+    } else {
+      // If all questions have been answered then display results
+      displayFinalScreen();
+    }
+    // Navigates to Question / Answer / Final Screens
+  });
+}
+
+function displayFinalScreen() {
+  $('.pose').prop("src", "https://uploads-ssl.webflow.com/5b44edefca321a1e2d0c2aa6/5c1485620de6e6e40adffb22_Dimensions-Guide-Humans-Standing-Icon.svg");
+  $('.pose').prop("alt", "Man and Woman Standing");
+  // Returns to original picture and alt text
+
+  const resultsText = [
+    `"There are two mistakes one can make along the road to truth: not going all the way, and not starting." – Buddha`,
+    `“Quiet the mind,and the soul will speak.” – Ma Jaya Sati Bhagavati`,
+    '“Change is not something that we should fear. Rather, it is something that we should welcome. For without change, nothing in this world would ever grow or blossom, and no one in this world would ever move forward to become the person they’re meant to be.” – B.K.S Iyengar',
+    `“Yoga is the journey of the self, through the self, to the self.” – The Bhagavad Gita`,
+    `“Yoga is not about touching your toes. It is what you learn on the way down.” – Jigar Gor`,
+    `“The pose begins when you want to get out of it.” – Baron Baptiste`,
+    `“Yoga teaches us to cure what need not be endured and endure what cannot be cured.” – B.K.S Iyengar`,
+    `“Yoga does not remove us from the reality or responsibilities of everyday life but rather places our feet firmly and resolutely in the practical ground of experience. We don’t transcend our lives; we return to the life we left behind in the hopes of something better.” – Donna Farhi`,
+    `“Inner peace begins the moment you choose not to allow another person or event to control your emotions." – Unknown`,
+    `“Yoga does not just change the way we see things, it transforms the person who sees.” – B.K.S Iyengar`,
+    `"Yoga is the stilling of the fluctuations of the mind" – Patanjali`
+  ]; // Quotes by score
+  
+  $('.the-answer').html(`
+    <h3>You scored a ${currentScore} out of 10</h3>
+  `); // Displays score
+
+  $('.question-form').html(`
+    <h5 class="quote">${resultsText[currentScore]}</h5><button type="button" class="submit">Play Again</button>
+  `); // Displays quote and updates button text
+
+  questionNumber = 0;
+  currentScore = 0;
+  answered = 0;
+  // Resets variables to play again
+}
+
+function widthChange(mq) {
+  if (mq.matches) {
+    $('.header').html(`
+      <h1 class="quiz-title">Yoga Quiz</h1>
+      <div>
+        <h2 class="bar js-question">Question: 0</h2>
+        <h2 class="bar js-score">Score: 0/0</h2>
+      </div>
+    `);
+  } else {
+    $('.header').html(`
+      <h2 class="bar js-question">Question: 0</h2>
+      <h1 class="quiz-title">Yoga Quiz</h1>
+      <h2 class="bar js-score">Score: 0/0</h2>
+    `);
+  }
+} // Handles screen size formatting
+
+function turnUpVolume() {
+  handleButtonPress();
+  // Informs page of the listener
+
+  if (matchMedia) {
+    const mq = window.matchMedia("(max-width: 430px)");
+    mq.addListener(widthChange);
+    widthChange(mq);
+  } // Manually add listener
+}
+
+$(turnUpVolume);
